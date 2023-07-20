2b0aef6b0dca2073fce908191504ae7858c2081c
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-07-20 23:41:02 +0900

    axios api 통신

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
-// 유저는 배너를 볼 수 있다.
-// 유저는 가장 인기있는 영화 리스트, 가장 평점 좋은 영화 리스트, 상영 예정작 리스트를 볼 수 있다.
-// 각각의 영화 리스트는 슬라이드 형태로 넘길 수 있다.
-// 유저는 영화에 마우스를 올려두면 영화의 제목과 장르, 평점, 인기도, 청불여부를 볼 수 있다.
-// 유저는 영화 카드를 클릭하면 영화의 상세 정보를 볼 수 있다.
-// 상세 정보에는 포스터, 영화 제목, 평점, 인기도 청불여부, 줄거리 요약, 예산, 날짜, 시간, 리뷰, 예고편, 관련영화 등이 있다.
-// 유저는 영화를 검색할 수 있다.
-// 유저는 영화를 인기도순으로 정렬할 수 있다.
-// 유저는 장르별로 영화를 필터링 할 수 있다.
-// 유저는 영화 날짜별로 필터링 할 수 있다.
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+import Navigation from "./components/Navigation";
+
+// 1. 3개의 페이지(홈,무비,무비 디테일)
+// 2. 유저는 홈페이지에서 배너를 볼 수 있다.
+// 3. 유저는 가장 인기있는 영화 리스트, 가장 평점 좋은 영화 리스트, 상영 예정작 리스트를 볼 수 있다.
+// 4. 각각의 영화 리스트는 슬라이드 형태로 넘길 수 있다.
+// 5. 유저는 영화에 마우스를 올려두면 영화의 제목과 장르, 평점, 인기도, 청불여부를 볼 수 있다.
+
+// 6. 유저는 영화 카드를 클릭하면 영화의 상세 정보를 볼 수 있다.(포스터, 영화 제목, 평점, 인기도 청불여부, 줄거리 요약, 예산, 날짜, 시간 등)
+// 7. trailer, 영화 리뷰, 관련된 영화도 볼 수 있음
+// 8. 유저는 영화를 검색할 수 있다.
+// 9. 유저는 영화를 인기도순으로 정렬할 수 있다.
+// 10. 유저는 영화를 필터링 할 수 있다.(장르별, 날짜별)
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+const Navigation = () => {
+  return (
+    <Navbar expand="lg" className="bg-dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img
+            width={100}
+            src="https://t3.ftcdn.net/jpg/04/81/76/22/360_F_481762281_Xcvl3QsGh1pBMvQuyKIoIqq8aYksXEwX.jpg"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Link to="/" className="nav-item">
+              Home
+            </Link>
+            <Link to="/movies" className="nav-item">
+              Movies
+            </Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
