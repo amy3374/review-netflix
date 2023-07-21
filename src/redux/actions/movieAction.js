18b58c046e13f53c034db736f96d80bb19815d6f
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-07-21 23:38:58 +0900

    axios api 통신

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -6,6 +6,14 @@ function getMovies() {
     const popularMovieApi = await api.get(
       `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
+    const topRatedApi = await api.get(
+      `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const upComingApi = await api.get(
+      `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    );
+
+    let data = Promise.all([popularMovieApi,topRatedApi,upComingApi])
   };
 }
 export const movieAction = { getMovies };
